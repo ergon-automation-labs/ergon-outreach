65e0d6129f956f24901b4c1d663f012d0dc2b43d
Abby Malson <august.malson@gmail.com>
2026-06-25 09:31:05 -0600

    fix: Update runtime.exs to use DATABASE_* variables (standard pattern)

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,20 +3,24 @@ import Config
 if config_env() == :prod do
   config :bot_army_outreach, BotArmyOutreach.Repo,
     username:
-      System.get_env("BOT_ARMY_OUTREACH_DB_USER") || System.get_env("DB_USER") || "bot_army",
+      System.get_env("BOT_ARMY_OUTREACH_DATABASE_USER") || System.get_env("DATABASE_USER") ||
+        "bot_army",
     password:
-      System.get_env("BOT_ARMY_OUTREACH_DB_PASSWORD") || System.get_env("DB_PASSWORD") ||
+      System.get_env("BOT_ARMY_OUTREACH_DATABASE_PASSWORD") || System.get_env("DATABASE_PASSWORD") ||
         "bot_army_password",
     database:
-      System.get_env("BOT_ARMY_OUTREACH_DB_NAME") || System.get_env("DB_NAME") || "ergon_outreach",
+      System.get_env("BOT_ARMY_OUTREACH_DATABASE_NAME") || System.get_env("DATABASE_NAME") ||
+        "ergon_outreach",
     hostname:
-      System.get_env("BOT_ARMY_OUTREACH_DB_HOST") || System.get_env("DB_HOST") || "localhost",
+      System.get_env("BOT_ARMY_OUTREACH_DATABASE_HOST") || System.get_env("DATABASE_HOST") ||
+        "localhost",
     port:
       String.to_integer(
-        System.get_env("BOT_ARMY_OUTREACH_DB_PORT") || System.get_env("DB_PORT") || "5432"
+        System.get_env("BOT_ARMY_OUTREACH_DATABASE_PORT") || System.get_env("DATABASE_PORT") ||
+          "5432"
       ),
     ssl:
-      System.get_env("BOT_ARMY_OUTREACH_DB_SSL") || System.get_env("DB_SSL") ||
+      System.get_env("BOT_ARMY_OUTREACH_DATABASE_SSL") || System.get_env("DATABASE_SSL") ||
         "false" |> String.to_atom(),
     pool_size: String.to_integer(System.get_env("BOT_POOL_SIZE", "10"))
 end
